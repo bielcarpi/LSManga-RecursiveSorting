c3732e06b494f5e54135d5ed22afb17ab2e6af0e
Biel Carpi <biel.carpi@students.salle.url.edu>
2021-11-26 16:14:01 +0100

    Fix files naming

--- src/Dataset.java
+++ src/Dataset.java
@@ -12,12 +12,12 @@
  * <p>The ones that will always exist will be the S, M and L datasets. Other ones need to be build previously.</p>
  */
 public enum Dataset {
-    S(Dataset.relativePath + "series_s.json", 50),
-    M(Dataset.relativePath + "series_m.json", 22450),
-    L(Dataset.relativePath + "series_l.json", 69400),
-    XL(Dataset.relativePath + "series_xl.json", 100000),
-    XXL(Dataset.relativePath + "series_xxl.json", 250000),
-    XXXL(Dataset.relativePath + "series_xxxl.json", 500000);
+    S(Dataset.relativePath + "series_S.json", 50),
+    M(Dataset.relativePath + "series_M.json", 22450),
+    L(Dataset.relativePath + "series_L.json", 69400),
+    XL(Dataset.relativePath + "series_XL.json", 100000),
+    XXL(Dataset.relativePath + "series_XXL.json", 250000),
+    XXXL(Dataset.relativePath + "series_XXXL.json", 500000);
 
     static final String relativePath = "resources/";
     String path;
