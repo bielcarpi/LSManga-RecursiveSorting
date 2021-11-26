8b67f08bf413eee3b67b01e75369cd0cad3586b5
Biel Carpi <biel.carpi@students.salle.url.edu>
2021-11-26 14:25:44 +0100

    Set numElements of each Dataset

--- src/Dataset.java
+++ src/Dataset.java
@@ -13,17 +13,18 @@
  * <p>The ones that will always exist will be the S, M and L datasets. Other ones need to be build previously.</p>
  */
 public enum Dataset {
-    XS(Dataset.relativePath + "series_xs.json"),
-    S(Dataset.relativePath + "series_s.json"),
-    M(Dataset.relativePath + "series_m.json"),
-    L(Dataset.relativePath + "series_l.json"),
-    XL(Dataset.relativePath + "series_xl.json"),
-    XXL(Dataset.relativePath + "series_xxl.json"),
-    XXXL(Dataset.relativePath + "series_xxxl.json");
+    S(Dataset.relativePath + "series_s.json", 50),
+    M(Dataset.relativePath + "series_m.json", 22450),
+    L(Dataset.relativePath + "series_l.json", 69400),
+    XL(Dataset.relativePath + "series_xl.json", 100000),
+    XXL(Dataset.relativePath + "series_xxl.json", 250000),
+    XXXL(Dataset.relativePath + "series_xxxl.json", 500000);
 
     static final String relativePath = "resources/";
     String path;
-    Dataset(String path){
+    int numElements;
+    Dataset(String path, int numElements){
         this.path = path;
+        this.numElements = numElements;
     }
 }
