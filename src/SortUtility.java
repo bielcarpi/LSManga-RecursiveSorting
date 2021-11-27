93a56f6ace618c50d2e9258f18d9709dd816f338
Biel Carpi <biel.carpi@students.salle.url.edu>
2021-11-27 08:57:48 +0100

    Add mergeSort() javadoc

--- src/SortUtility.java
+++ src/SortUtility.java
@@ -13,7 +13,12 @@ import java.util.Comparator;
 public class SortUtility<T> {
 
     /**
+     * Orders (using merge sort) an array of {@code T Objects} given a {@link Comparator<T>} for that same {@code object}
+     * <p>The array passed is the one that will be modified, this method is void.
      *
+     * @see <a href="https://en.wikipedia.org/wiki/Merge_sort">Merge Sort</a>
+     * @param array The array that will be ordered
+     * @param comparator The criteria that will order the array
      */
     public void mergeSort(T[] array, Comparator<T> comparator){
     }
