a70a51e06a43acc1615aecfe10f74f5b902662cf
Biel Carpi <biel.carpi@students.salle.url.edu>
2021-11-27 08:50:33 +0100

    Add class SortUtility

---
+++ src/SortUtility.java
@@ -0,0 +1,20 @@
+import java.util.Comparator;
+
+/**
+ * {@code SortUtility} is a class designed to help with sorting arrays of {@code Objects}.
+ * <p>As we want to order an Object T, we'll use {@code Generics}.
+ * <br>
+ * <p>The class will provide methods for sorting an array with different algorithms, such as merge sort, quick sort or bucket sort
+ *
+ * @see <a href="https://docs.oracle.com/javase/tutorial/java/generics/types.html">Generics</a>
+ * @author bielcarpi
+ * @version 1.0
+ */
+public class SortUtility<T> {
+
+    /**
+     *
+     */
+    public void mergeSort(T[] array, Comparator<T> comparator){
+    }
+}
