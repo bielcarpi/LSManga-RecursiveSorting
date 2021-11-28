16793bea517d8dba1d81c84aaf43c1f63526fd52
Biel Carpi <biel.carpi@students.salle.url.edu>
2021-11-28 07:55:06 +0100

    Modify main so as to see it working

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ public class Main {
     public static void main(String[] args){
         SeriesManager seriesManager = SeriesManager.getSeriesManager();
         seriesManager.loadDataset(Dataset.S);
-        seriesManager.orderLoadedDatasetWithMergeSort();
+        seriesManager.orderLoadedDatasetWithQuicksort();
         seriesManager.printSeriesLoaded();
     }
 
