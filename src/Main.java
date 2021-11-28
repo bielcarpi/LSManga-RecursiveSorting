c82019cd21269a49780e6794746c90b5eee9d1d7
Biel Carpi <biel.carpi@students.salle.url.edu>
2021-11-28 10:12:03 +0100

    Modify main() so as to adapt to the new orderLoadedDataset()

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ public class Main {
     public static void main(String[] args){
         SeriesManager seriesManager = SeriesManager.getSeriesManager();
         seriesManager.loadDataset(Dataset.S);
-        seriesManager.orderLoadedDatasetWithQuicksort();
+        seriesManager.orderLoadedDataset(SeriesManager.SeriesComparator.BY_PREMIERE_DATE, SortUtility.SortType.QUICKSORT);
         seriesManager.printSeriesLoaded();
     }
 
