b33ad8bdef9e70bc593e51a53989d277f87dcd9e
Biel Carpi <biel.carpi@students.salle.url.edu>
2021-11-28 18:22:33 +0100

    Add basic explanation of the program on main()

--- src/Main.java
+++ src/Main.java
@@ -8,10 +8,20 @@
 public class Main {
 
     public static void main(String[] args){
+        //Get Instance of the SeriesManager class. It will provide methods for generating, loading, writing and ordering datasets
         SeriesManager seriesManager = SeriesManager.getSeriesManager();
-        seriesManager.generateDataset(Dataset.XXXXL);
-        seriesManager.orderLoadedDataset(SeriesManager.SeriesComparator.BY_POPULARITY, SortUtility.SortType.BUCKET_SORT);
-        //seriesManager.printSeriesLoaded();
+
+        //There are 7 types of datasets: S, M, L, XL, ..., XXXXL (see the Datset enum)
+        seriesManager.generateDataset(Dataset.XXXXL); //Generate a dataset, load it into memory and write it
+        //seriesManager.loadDataset(Dataset.S); //Or load an already created dataset
+
+        //Order the loaded dataset with a comparator (By popularity, premiere date or total score) and one of the
+        //  sorting algorithms (either merge sort, quicksort or bucket sort).
+        //  Take into account that Bucketsort can only order By Popularity. The other algorithms, can order by any comparator.
+        seriesManager.orderLoadedDataset(SeriesManager.SeriesComparator.BY_POPULARITY, SortUtility.SortType.QUICKSORT);
+
+        //seriesManager.printSeriesLoaded(); //Either print the ordered series on stdin
+        //seriesManager.writeCurrentDataset(); //Or write the ordered series on its file
     }
 
 }
